fcd1a9a1469d7378315de58983e44553f447eae3
Ryan Lynch <lynch.r.j@gmail.com>
2016-07-10 07:36:32 -0400

    Removed uneeded share call from ReTweet action

--- examples/retweet/client/actions/socket.js
+++ examples/retweet/client/actions/socket.js
@@ -4,6 +4,5 @@ import { createAction } from 'redurx';
 export const initializeSocket = createAction(init => {
   return init
     .take(1)
-    .map(() => io())
-    .share();
+    .map(() => io());
 });
